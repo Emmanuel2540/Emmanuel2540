603d8401fe1b61a2b361cf96706b638b5ca6ed8a
Emmanuel2540 <keinsteins246@gmail.com>
2024-11-09 21:22:47 +0300

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include <stdio .h>
+
+int main(viod)
+{
+    int b;
+    printf("3:");
+    scanf("%d" , &b );
+    if (b < 0)
+    {
+        printf("The value is negative\n");
+    }
+   return 0;
+
+
+}
+
